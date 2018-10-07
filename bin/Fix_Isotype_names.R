8583ece1aa1a791353da87e55cf409e99413a0a3
Stefan Zdraljevic <szdralje@gmail.com>
2018-10-07 15:47:36 -0500

    add script to fix isotype names

---
+++ bin/Fix_Isotype_names.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+library(tidyverse)
+library(cegwas2)
+library(data.table)
+
+
+for(i in grep(".tsv",list.files(), value = T)){
+  
+  temp_file <- data.table::fread(i)
+  
+  trt <- colnames(temp_file][2]
+
+  pr_trt <- cegwas2::process_phenotypes(temp_file)
+
+  readr::write_tsv(pr_trt, glue::glue("{trt}.tsv"))
+}
